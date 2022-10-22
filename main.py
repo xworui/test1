00478c0a67f17b069b9392d4f789dc709f08e6b9
gaidukov <gaidukov_nikita@mail.ru>
2022-10-22 22:23:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print('Введите n')
+
+n = int(input())
+
+
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    F1 = 0
+    F2 = 1
+    i = 1
+    Fn = 0
+    while n > i:
+        Fn = F1 + F2
+        F1, F2 = F2, Fn
+        i += 1
+print(Fn)
